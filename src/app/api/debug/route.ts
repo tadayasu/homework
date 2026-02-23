597e22f7e59ebb6280bf76b66b0e8283782c401f
四津匡康 <yotsu@mac.lan>
2026-02-23 20:52:40 +0900

    Remove temporary debug endpoint

--- src/app/api/debug/route.ts
+++
@@ -1,8 +0,0 @@
-export function GET() {
-  return Response.json({
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL ?? "未設定",
-    NEXTAUTH_URL_set: !!process.env.NEXTAUTH_URL,
-    GOOGLE_CLIENT_ID_set: !!process.env.GOOGLE_CLIENT_ID,
-    NEXTAUTH_SECRET_set: !!process.env.NEXTAUTH_SECRET,
-  })
-}
